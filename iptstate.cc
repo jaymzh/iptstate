a7164c6299a66688bf40360fdc58431ddf64e9e8
Phil Dibowitz <phil@ipom.com>
2012-06-02 04:20:44 +0000

    Changelog, version bump.

--- iptstate.cc
+++ iptstate.cc
@@ -63,7 +63,7 @@ extern "C" {
 #include <unistd.h>
 using namespace std;
 
-#define VERSION "2.2.4+CVS"
+#define VERSION "2.2.5"
 /*
  * MAXCONS is set to 16k, the default number of states in iptables. Generally
  * speaking the ncurses pad is this many lines long, but since ncurses
